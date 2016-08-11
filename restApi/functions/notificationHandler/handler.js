a6be77b8e657ef6fe6dfc0f2a4b4fe2f6470360d
Elliott Spira <elliottspira@gmail.com>
2016-08-11 13:43:01 +1000

    adding new notificationHandler

--- restApi/functions/notificationHandler/handler.js
+++ restApi/functions/notificationHandler/handler.js
@@ -7,8 +7,10 @@ const validateJWT = require('../../lib/jwt').validateJWT;
 
 exports.handler = function(event, context, cb) {
   lib.logger.debug('Event json:', JSON.stringify(event));
+  const eventData = event.toString();
+  lib.logger.debug('Event data:', eventData);
   const hipchat = new HipChatAPI(lib.dbManager, lib.logger);
-  
+
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
   });
