c8d1b642a6f4b2515899da9efda601915f490bf7
Elliott Spira <elliottspira@gmail.com>
2016-08-11 13:53:12 +1000

    adding sns notification manager

--- restApi/functions/notificationHandler/handler.js
+++ restApi/functions/notificationHandler/handler.js
@@ -4,6 +4,7 @@
 const lib = require('../../lib/index').default();
 const HipChatAPI = require('../../lib/hipchat_api').HipChatAPI;
 const validateJWT = require('../../lib/jwt').validateJWT;
+const handleNotification = require('../../lib/sns_notification_manager').handleNotification;
 
 exports.handler = function(event, context, cb) {
   lib.logger.debug('Event json:', JSON.stringify(event));
@@ -11,7 +12,8 @@ exports.handler = function(event, context, cb) {
   lib.logger.debug('Event data:', eventData);
   const hipchat = new HipChatAPI(lib.dbManager, lib.logger);
 
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
+  handleNotification(hipchat, data).then(
+    result => cb(null, { message: 'Go Serverless! Your Lambda function executed successfully!' }),
+    err => cb(err)
+  );
 };
