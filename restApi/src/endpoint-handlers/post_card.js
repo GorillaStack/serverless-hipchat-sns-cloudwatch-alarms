757b5a05bff8a656d6e9b5e5a503cd6b92efbbd8
Elliott Spira <elliottspira@gmail.com>
2016-10-11 15:51:47 +1100

    running eslint over new code

--- restApi/src/endpoint-handlers/post_card.js
+++ restApi/src/endpoint-handlers/post_card.js
@@ -1,3 +1,58 @@
+const getLozengeStyleForStatus = status => {
+  if (status === 'OK') {
+    return 'lozenge-success';
+  } else if (status === 'INSUFFICIENT_DATA') {
+    return 'lozenge-current';
+  } else if (status === 'ALARM') {
+    return 'lozenge-error';
+  }
+
+  return 'lozenge-current';
+};
+
+const formatMessageForAlarm = (alarm, user) => ({
+  color: 'gray',
+  // this is a backup message for HipChat clients that do not understand cards
+  // (old HipChat clients, 3rd party XMPP clients)
+  message: `Alarm being escalated: ${JSON.stringify(alarm)}`,
+  message_format: 'text',
+  card: {
+    style: 'application',
+    id: 'some_id',
+    format: 'medium',
+    title: `CloudWatch Alarm escalated by ${user}`,
+    description: alarm.NewStateReason,
+    attributes: [
+      {
+        label: 'Alarm Name',
+        value: {
+          label: alarm.AlarmName,
+        },
+      },
+      {
+        label: 'Alarm Description',
+        value: {
+          label: alarm.AlarmDescription,
+        },
+      },
+      {
+        label: 'Previous State',
+        value: {
+          label: alarm.OldStateValue,
+          style: getLozengeStyleForStatus(alarm.OldStateValue),
+        },
+      },
+      {
+        label: 'Current State',
+        value: {
+          label: alarm.NewStateValue,
+          style: getLozengeStyleForStatus(alarm.NewStateValue),
+        },
+      },
+    ],
+  },
+});
+
 const handler = (lib, hipchat, event, oauthData) => new Promise((resolve, reject) => {
   try {
     lib.logger.debug('In /post-card handler');
@@ -5,8 +60,8 @@ const handler = (lib, hipchat, event, oauthData) => new Promise((resolve, reject
     lib.logger.debug('requrest body for card', event.body);
     const message = formatMessageForAlarm(event.body.alarm, event.body.user);
     hipchat.sendMessage(oauthData.oauthId, oauthData.roomId, message).then(
-      (res) => resolve(res),
-      (err) => {
+      res => resolve(res),
+      err => {
         lib.logger.error('Could not run /post-card handler', err);
         reject(err);
       }
@@ -16,58 +71,5 @@ const handler = (lib, hipchat, event, oauthData) => new Promise((resolve, reject
   }
 });
 
-const formatMessageForAlarm = (alarm, user) => {
-  return {
-    color: 'gray',
-    // this is a backup message for HipChat clients that do not understand cards (old HipChat clients, 3rd party XMPP clients
-    message: 'Alarm being escalated: '  + JSON.stringify(alarm),
-    message_format: 'text',
-    card: {
-      style: 'application',
-      id: 'some_id',
-      format: 'medium',
-      title: 'CloudWatch Alarm escalated by ' + user,
-      description: alarm.NewStateReason,
-      attributes: [
-        {
-          label: 'Alarm Name',
-          value: {
-            label: alarm.AlarmName
-          }
-        },
-        {
-          label: 'Alarm Description',
-          value: {
-            label: alarm.AlarmDescription
-          }
-        },
-        {
-          label: 'Previous State',
-          value: {
-            label: alarm.OldStateValue,
-            style: getLozengeStyleForStatus(alarm.OldStateValue)
-          }
-        },
-        {
-          label: 'Current State',
-          value: {
-            label: alarm.NewStateValue,
-            style: getLozengeStyleForStatus(alarm.NewStateValue)
-          }
-        }
-      ]
-    }
-  };
-}
-
-const getLozengeStyleForStatus = status => {
-  if (status === 'OK') {
-    return 'lozenge-success';
-  } else if (status === 'INSUFFICIENT_DATA') {
-    return 'lozenge-current';
-  } else if (status === 'ALARM') {
-    return 'lozenge-error';
-  }
-};
 
 export default handler;
--- restApi/src/endpoint-handlers/topics.js
+++ restApi/src/endpoint-handlers/topics.js
@@ -1,34 +1,7 @@
-'use strict';
-
 import { getSummaryStateForAlarms } from '../topic_group_manager';
 
-const handler = (lib, hipchat, event, oauthData) => {
-  return new Promise((resolve, reject) => {
-    try {
-      lib.logger.debug('In /topics handler');
-      lib.logger.debug('topicGroupKey: ' + JSON.stringify(event));
-      lib.dbManager.query(process.env.ALARM_TABLE, 'topicGroupKey', event.body.topicGroupKey)
-        .then(result => {
-          lib.logger.debug('result.Items', { data: JSON.stringify(result.Items) });
-          const alarmsByTopic = getAlarmsByTopic(lib, result.Items);
-
-          resolve({
-            status: 'success',
-            topics: alarmsByTopic
-          });
-        }, err => {
-          lib.logger.error('Could not retrieve alarms for topicGroupKey: ' + event.body.topicGroupKey,
-            { msg: err.toString(), stack: err.stack });
-          reject(err);
-        });
-    } catch (err) {
-      reject(err);
-    }
-  });
-};
-
 const getAlarmsByTopic = (lib, alarms) => {
-  let topics = {};
+  const topics = {};
   alarms.forEach(entry => {
     if (typeof topics[entry.topicName] === 'undefined') {
       topics[entry.topicName] = [];
@@ -37,13 +10,33 @@ const getAlarmsByTopic = (lib, alarms) => {
     topics[entry.topicName].push(entry.alarm);
   });
 
-  return Object.keys(topics).map(key => {
-    return {
-      title: key,
-      alarms: topics[key],
-      status: getSummaryStateForAlarms(lib, topics[key])
-    };
-  });
+  return Object.keys(topics).map(key => ({
+    title: key,
+    alarms: topics[key],
+    status: getSummaryStateForAlarms(lib, topics[key]),
+  }));
 };
+
+const handler = (lib, hipchat, event) => new Promise((resolve, reject) => {
+  try {
+    lib.logger.debug('In /topics handler');
+    lib.logger.debug(`topicGroupKey: ${JSON.stringify(event)}`);
+    lib.dbManager.query(process.env.ALARM_TABLE, 'topicGroupKey', event.body.topicGroupKey)
+      .then(result => {
+        lib.logger.debug('result.Items', { data: JSON.stringify(result.Items) });
+        const alarmsByTopic = getAlarmsByTopic(lib, result.Items);
+        resolve({
+          status: 'success',
+          topics: alarmsByTopic,
+        });
+      }, err => {
+        lib.logger.error(`Could not retrieve alarms for topicGroupKey: ${event.body.topicGroupKey}`,
+          { msg: err.toString(), stack: err.stack });
+        reject(err);
+      });
+  } catch (err) {
+    reject(err);
+  }
+});
 
 export default handler;
